6cab608adce4736317601d427404b7153d6467b3
rafael <r.victorio@aluno.ufabc.edu.br>
2024-02-09 09:38:26 -0300

    1.1 - Criação do projeto e adição das lógicas básicas referente ao CRUD

---
+++ src/main/java/com/rafaelvictorio/todolist/repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package com.rafaelvictorio.todolist.repository;
+
+import com.rafaelvictorio.todolist.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+}
---
+++ src/main/java/com/rafaelvictorio/todolist/service/TodoService.java
@@ -0,0 +1,36 @@
+package com.rafaelvictorio.todolist.service;
+
+import com.rafaelvictorio.todolist.entity.Todo;
+import com.rafaelvictorio.todolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+       Sort sort= Sort.by("prioridade").descending().and(Sort.by("nome").ascending());
+       return todoRepository.findAll(sort);
+    }
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> delete(Long id){
+        todoRepository.delete(null);
+        return list();
+    }
+
+}
